1ced49a4d00cd74017ffe606585b23cfdf91a372
Octivian <408199559@qq.com>
2015-08-22 22:58:49 +0800

    count distinct ip and uv

--- src/main/java/com/dsp/statistics/CountDistincTest.scala
+++ src/main/java/com/dsp/statistics/CountDistincTest.scala
@@ -22,17 +22,20 @@ object CountDistincTest {
      */
     rdd1.combineByKey(
       (v: String) => {
-        List(v.split("\t")(2))
+        List(List(v.split("\t")(2)),List(v.split("\t")(6)))
       },
-      (c: List[String],v: String) => {
-        v.split("\t")(2)::c
+      (c: List[List[String]],v: String) => {
+        List(v.split("\t")(2)::c(0),v.split("\t")(6)::c(1))
       },
-      (c1: List[String],c2: List[String])=>{
-        c1:::c2
+      (c1: List[List[String]],c2: List[List[String]])=>{
+        List(c1(0):::c2(0),c1(1):::c2(1))
       }
-    ).map(kv=>(kv._1,kv._2.distinct.length)).saveAsTextFile(args(1))
+    ).map(kv=>{
+      (kv._1,(kv._2(0).distinct.length,kv._2(1).distinct.length))
+    }).saveAsTextFile(args(1))
 
   }
+
 
   private def filterDatas(lines: String): Boolean = {
     lines.split("\t")(1).equals("21")||lines.split("\t")(1).equals("22")
